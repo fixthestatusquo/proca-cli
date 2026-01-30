039329e442e8483742c8398663d218471fcf5c82
1v4n4 <ivananl@yahoo.com>
2026-01-30 01:21:44 +0100

    Modify add to be able to accept config prop

--- src/commands/campaign/add.mjs
+++ src/commands/campaign/add.mjs
@@ -6,96 +6,108 @@ import { gql, mutation } from "#src/urql.mjs";
 import { getTwitter } from "#src/util/twitter.mjs";
 
 export default class CampaignAdd extends Command {
-	static args = {
-		title: Args.string({
-			description: "title of the campaign",
-			multiple: true,
-		}),
-	};
-	//  static strict = false;
+  static args = {
+    title: Args.string({
+      description: "title of the campaign",
+      multiple: true,
+    }),
+  };
+  //  static strict = false;
 
-	static examples = [
-		"<%= config.bin %> <%= command.id %> -n <new_campaign> the full name of the campaign",
-	];
+  static examples = [
+    "<%= config.bin %> <%= command.id %> -n <new_campaign> the full name of the campaign",
+  ];
 
-	static flags = {
-		// flag with no value (-f, --force)
-		...super.globalFlags,
-		name: Flags.string({
-			char: "n",
-			description: "name of the campaign",
-			helpValue: "<campaign name>",
-			required: true,
-		}),
-		org: Flags.string({
-			char: "o",
-			description: "name of the coordinator",
-			helpValue: "<org name>",
-			required: true,
-		}),
-	};
+  static flags = {
+    // flag with no value (-f, --force)
+    ...super.globalFlags,
+    name: Flags.string({
+      char: "n",
+      description: "name of the campaign",
+      helpValue: "<campaign name>",
+      required: true,
+    }),
+    org: Flags.string({
+      char: "o",
+      description: "name of the coordinator",
+      helpValue: "<org name>",
+      required: true,
+    }),
+  };
 
-	create = async (campaign) => {
-		const org = await this.getOrg(campaign.org);
+  create = async (campaign, customConfig = null) => {
+    const org = await this.getOrg(campaign.org);
 
-		const config = {
-			locales: {
-				en: {
-					"campaign:": {
-						description: "",
-					},
-					"common:": {},
-				},
-			},
-		};
-		if (org.config.locale && org.config.locale !== "en") {
-			config.locales[org.config.locale] = {};
-		}
-		if (!config.portal) {
-			config.portal = [];
-		}
-		const AddOrgDocument = gql`
-mutation ($org: String!
-$name: String!
-$title: String!
-$config: Json!
-) {
-  addCampaign (input: { name: $name, title: $title, config: $config }, orgName: $org) {
-    name
-    title
-    config
-  }
-}
+    let config;
+
+    // Use custom config if provided, otherwise create default
+    if (customConfig) {
+      config =
+        typeof customConfig === "string"
+          ? JSON.parse(customConfig)
+          : customConfig;
+    } else {
+      // Default config creation (existing functionality)
+      config = {
+        locales: {
+          en: {
+            "campaign:": {
+              description: "",
+            },
+            "common:": {},
+          },
+        },
+      };
+
+      if (org.config.locale && org.config.locale !== "en") {
+        config.locales[org.config.locale] = {};
+      }
+
+      if (!config.portal) {
+        config.portal = [];
+      }
+    }
+
+    const AddOrgDocument = gql`
+        mutation ($org: String!
+        $name: String!
+        $title: String!
+        $config: Json!
+        ) {
+          addCampaign (input: { name: $name, title: $title, config: $config }, orgName: $org) {
+            name
+            title
+            config
+          }
+        }
     `;
-		const result = await mutation(AddOrgDocument, {
-			org: org.name,
-			name: campaign.name,
-			title: campaign.title,
-			config: JSON.stringify(config),
-		});
 
-		console.log("result", result);
-		return result;
-	};
+    const result = await mutation(AddOrgDocument, {
+      org: org.name,
+      name: campaign.name,
+      title: campaign.title,
+      config: JSON.stringify(config),
+    });
 
-	async getOrg(orgName) {
-		const { config } = this;
-		const orgGet = new OrgGet({}, this.config);
-		const org = await orgGet.fetch({ name: orgName });
-		return org;
-	}
+    console.log("result", result);
+    return result;
+  };
 
-	async run() {
-		//const { args, flags } = await this.parse(CampaignAdd);
-		const { args, flags } = await this.parse();
+  async getOrg(orgName) {
+    const { config } = this;
+    const orgGet = new OrgGet({}, this.config);
+    const org = await orgGet.fetch({ name: orgName });
+    return org;
+  }
 
-		const campaign = {
-			org: flags.org,
-			name: flags.name,
-			title: args.title || flags.name,
-		};
-
-		const data = await this.create(campaign);
-		return this.output(data);
-	}
+  async run() {
+    const { args, flags } = await this.parse();
+    const campaign = {
+      org: flags.org,
+      name: flags.name,
+      title: args.title || flags.name,
+    };
+    const data = await this.create(campaign);
+    return this.output(data);
+  }
 }
