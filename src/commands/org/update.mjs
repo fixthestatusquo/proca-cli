4565d046990e5a6f09fdc144326959c317b3cb4b
1v4n4 <ivananl@yahoo.com>
2026-05-20 14:04:52 +0200

    Add new org params in org update

--- src/commands/org/update.mjs
+++ src/commands/org/update.mjs
@@ -3,6 +3,19 @@ import Command from "#src/procaCommand.mjs";
 import { gql, mutation } from "#src/urql.mjs";
 
 export default class OrgUpdate extends Command {
+  static description = "Update organisation settings";
+
+  static examples = [
+    "<%= config.bin %> <%= command.id %> myorg --title='My Organisation'",
+    "<%= config.bin %> <%= command.id %> myorg --reply-enabled",
+    "<%= config.bin %> <%= command.id %> myorg --no-reply-enabled",
+    "<%= config.bin %> <%= command.id %> myorg --sender-rewrite",
+    "<%= config.bin %> <%= command.id %> myorg --no-sender-rewrite",
+    "<%= config.bin %> <%= command.id %> myorg --doi-thank-you",
+    "<%= config.bin %> <%= command.id %> myorg --no-doi-thank-you",
+    "<%= config.bin %> <%= command.id %> myorg --title='My Org' --no-sender-rewrite --reply-enabled",
+  ];
+
   static args = this.namearg();
 
   static flags = {
@@ -11,21 +24,38 @@ export default class OrgUpdate extends Command {
       char: "t",
       description: "title/full name of the org",
       helpValue: "<org full name>",
-      required: true,
+    }),
+    "reply-enabled": Flags.boolean({
+      description: "enable reply_to for emails",
+      allowNo: true,
+    }),
+    "sender-rewrite": Flags.boolean({
+      description:
+        "rewrite sender address using SRS (disable for cleaner confirmation emails)",
+      allowNo: true,
+    }),
+    "doi-thank-you": Flags.boolean({
+      description: "only send thank you emails to opt-ins",
+      allowNo: true,
     }),
   };
 
   update = async (name, org) => {
     const Document = gql`
-mutation ($name: String!,$org: OrgInput!) {
-  updateOrg(name: $name,input: $org) {
-    id
-    name
-    title
-    config
-  }
-}
+      mutation ($name: String!, $org: OrgInput!) {
+        updateOrg(name: $name, input: $org) {
+          id
+          name
+          title
+          personalData {
+            replyEnabled
+            senderRewrite
+            doiThankYou
+          }
+        }
+      }
     `;
+
     const result = await mutation(Document, { name, org });
     if (!result.updateOrg) {
       console.log(result);
@@ -34,12 +64,33 @@ mutation ($name: String!,$org: OrgInput!) {
     return result.updateOrg;
   };
 
+  simplify = (d) => ({
+    id: d.id,
+    name: d.name,
+    title: d.title,
+    replyEnabled: d.personalData?.replyEnabled,
+    senderRewrite: d.personalData?.senderRewrite,
+    doiThankYou: d.personalData?.doiThankYou,
+  });
+
   async run() {
     const { flags } = await this.parse();
 
-    const org = {
-      title: flags.title,
-    };
+    const org = {};
+
+    if (flags.title) org.title = flags.title;
+    if (flags["reply-enabled"] !== undefined)
+      org.replyEnabled = flags["reply-enabled"];
+    if (flags["sender-rewrite"] !== undefined)
+      org.senderRewrite = flags["sender-rewrite"];
+    if (flags["doi-thank-you"] !== undefined)
+      org.doiThankYou = flags["doi-thank-you"];
+
+    if (Object.keys(org).length === 0) {
+      this.error(
+        "Provide at least one field to update (--title, --reply-enabled, --sender-rewrite, --doi-thank-you)",
+      );
+    }
 
     const data = await this.update(flags.name, org);
     return this.output(data);
