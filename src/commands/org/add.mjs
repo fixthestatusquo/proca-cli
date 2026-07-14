0e4a2c6f53e1e4b8768b4e916b4e17f3a4f8516f
1v4n4 <ivananl@yahoo.com>
2026-07-14 16:19:49 +0200

    When create org, add default efrom and email backend

--- src/commands/org/add.mjs
+++ src/commands/org/add.mjs
@@ -1,5 +1,6 @@
 import { Args, Flags } from "@oclif/core";
 import { error, stdout, ux } from "@oclif/core/ux";
+import { SERVICE_NAMES } from "#src/commands/org/email.mjs";
 import Command from "#src/procaCommand.mjs";
 import { gql, mutation } from "#src/urql.mjs";
 import { getTwitter } from "#src/util/twitter.mjs";
@@ -14,28 +15,61 @@ export default class OrgAdd extends Command {
       description: "title/full name of the org",
       helpValue: "<org full name>",
     }),
+    email: Flags.string({
+      char: "e",
+      description: "email address of the org",
+      helpValue: "<org email address>",
+    }),
+    mailer: Flags.string({
+      description: "service to send emails",
+      options: SERVICE_NAMES,
+      helpValue: SERVICE_NAMES,
+      default: "system",
+    }),
   };
 
-  create = async (_org) => {
+  create = async (_org, emailFrom, emailBackend) => {
     const org = { ..._org, config: JSON.stringify(_org.config) };
 
     const AddOrgDocument = gql`
-mutation ($org: OrgInput!) {
-  addOrg(input: $org) {
-    config
-    name
-    title
-  }
-}
+      mutation (
+        $org: OrgInput!
+        $name: String!
+        $emailBackend: ServiceName
+        $emailFrom: String
+      ) {
+        addOrg(input: $org) {
+          config
+          name
+          title
+        }
+        updateOrgProcessing(
+          name: $name
+          emailBackend: $emailBackend
+          emailFrom: $emailFrom
+        ) {
+          processing {
+            emailBackend
+            emailFrom
+          }
+        }
+      }
     `;
     const result = await mutation(AddOrgDocument, {
       org,
+      name: org.name,
+      emailBackend,
+      emailFrom,
     });
     if (!result.addOrg) {
       console.log(result);
       return result;
     }
-    return result?.addOrg;
+    return {
+      ...result.addOrg,
+      emailBackend: result.updateOrgProcessing?.processing.emailBackend,
+      emailFrom: result.updateOrgProcessing?.processing.emailFrom,
+    };
   };
 
   async run() {
@@ -47,7 +81,10 @@ mutation ($org: OrgInput!) {
       config: {},
     };
 
-    const data = await this.create(org);
+    const emailFrom = flags.email || `${flags.name}@proca.app`;
+    const emailBackend = flags.mailer.toUpperCase();
+
+    const data = await this.create(org, emailFrom, emailBackend);
     return this.output(data);
   }
 }
